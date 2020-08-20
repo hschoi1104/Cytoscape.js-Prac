4bc12c74a92238a14853dace3af238b8869f9c39
hschoi1104 <hschoi1104@gmail.com>
2020-08-20 16:50:27 +0900

    update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,30 @@
+const path = require("path")
+
 module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/scss/main.scss";`
+      }
+    }
+  },
   publicPath: "/",
-  outputDir: "../backend/public/",
+  outputDir : path.resolve(__dirname, "../backend/public/"),
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "http://nodejs:3000",
         changeOrigin: true,
         pathRewrite: { "^/apis": "" }
       }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/")
+      },
+      extensions: [".js", ".vue"]
     }
   }
 };
